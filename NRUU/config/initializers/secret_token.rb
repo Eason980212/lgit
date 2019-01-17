06b5dafa048ae409796b1a8d67f84812662799ac
sunhl <sunhl@dehuinet.com>
2019-01-17 17:45:02 +0800

    on clone all

---
+++ NRUU/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NRUU::Application.config.secret_key_base = 'a3af12c0cc5be21732c504c557f57165f534ba34e0b17e741013e7365d9e53cc3ebec9766ef977b75d10f625ad9211ff01e25855805e031509fa8d4152d444db'
---
+++ NRUU/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+NRUU::Application.config.session_store :cookie_store, key: '_NRUU_session'
